d5743704b56da2c521a8dc647e9c3a0e93e9fec3
lej99 <eunji_ouo@naver.com>
2023-01-03 17:37:11 +0900

    23.01.03(0)

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
 $(document).ready(function () {
 
-    var swiper = new Swiper(".slider_full", {
-        spaceBetween: 30,
-        centeredSlides: true,
-        autoplay: {
-          delay: 2500,
-          disableOnInteraction: false,
-        },
-        pagination: {
-          el: ".slide_pagination",
-          clickable: true,
-        },
-        navigation: {
-          nextEl: ".slide_next",
-          prevEl: ".slide_prev",
-        },
-      });
-
 });
