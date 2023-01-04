0c127b54f6b26e9e9c3266475dd7517bfa326a7e
lej99 <eunji_ouo@naver.com>
2023-01-04 17:58:07 +0900

    23.01.04(0)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
 
+    $('#slider .slider_control .pagination .swiper-pagination-bullet').eq(0)
+    .html('<span class="bullet_text">COMING SOON</span>');
+    $('#slider .slider_control .pagination .swiper-pagination-bullet').eq(1)
+    .html('<span class="bullet_text">NEW</span>');
+    $('#slider .slider_control .pagination .swiper-pagination-bullet').eq(2)
+    .html('<span class="bullet_text">DOOSUNG PAPER</span>');
+
+    
 });
