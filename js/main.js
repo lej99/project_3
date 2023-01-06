ab3733d32d5d0b3ae80d6197ef93f7dc47a735b0
lej99 <eunji_ouo@naver.com>
2023-01-06 17:16:00 +0900

    23.01.06(0)

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
 $(document).ready(function () {
 
-    $('#slider .slider_control .pagination .swiper-pagination-bullet').eq(0)
-    .html('<span class="bullet_text">COMING SOON</span>');
-    $('#slider .slider_control .pagination .swiper-pagination-bullet').eq(1)
-    .html('<span class="bullet_text">NEW</span>');
-    $('#slider .slider_control .pagination .swiper-pagination-bullet').eq(2)
+    $('.slider_control .pagination .swiper-pagination-bullet').eq(0)
+    .html('<span class="bullet_text">NEW-NEWUSE</span>');
+    $('.slider_control .pagination .swiper-pagination-bullet').eq(1)
+    .html('<span class="bullet_text">NEW-THE NATURAL</span>');
+    $('.slider_control .pagination .swiper-pagination-bullet').eq(2)
     .html('<span class="bullet_text">DOOSUNG PAPER</span>');
 
-    
+    $('.side_door').click(function(){
+        $('.close').toggleClass('off');
+        $('.open').toggleClass('off');
+
+        if($('#side_menu').css('margin-right') == '-400px') {
+            $('#side_menu').stop().animate({ marginRight: 0}, 400);
+        } else {
+            $('#side_menu').stop().animate({ marginRight: -400}, 400);
+        }
+        
+    });
+
 });
