f8117ed18b0b03d37340acab294e32bb6d497bbe
lej99 <eunji_ouo@naver.com>
2023-01-09 17:57:07 +0900

    23.01.09(0)

--- js/common.js
+++ js/common.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
 
+    // 광고슬라이드
+    let i = 0;
+    function addSlide(){
+        i++;
+        if(i>$('.add_text > li:last').index()){
+            i = 0;
+        }
+        $('.add_text > li').eq(i).stop().fadeIn('slow');
+        $('.add_text > li').eq(i-1).stop().fadeOut();
+    };
+    setInterval(addSlide,5000);
+
+    // 메뉴
     $('.top_menu > li').hover(function(){
         $(this).children('ul').show();
     }, function(){
--- js/main.js
+++ js/main.js
@@ -1,22 +1,28 @@
 $(document).ready(function () {
 
     $('.slider_control .pagination .swiper-pagination-bullet').eq(0)
-    .html('<span class="bullet_text">NEW-NEWUSE</span>');
+        .html('<span class="bullet_text">NEW-NEWUSE</span>');
     $('.slider_control .pagination .swiper-pagination-bullet').eq(1)
-    .html('<span class="bullet_text">NEW-THE NATURAL</span>');
+        .html('<span class="bullet_text">NEW-THE NATURAL</span>');
     $('.slider_control .pagination .swiper-pagination-bullet').eq(2)
-    .html('<span class="bullet_text">DOOSUNG PAPER</span>');
+        .html('<span class="bullet_text">DOOSUNG PAPER</span>');
 
-    $('.side_door').click(function(){
+    $('.side_door').click(function () {
         $('.close').toggleClass('off');
         $('.open').toggleClass('off');
 
-        if($('#side_menu').css('margin-right') == '-400px') {
-            $('#side_menu').stop().animate({ marginRight: 0}, 400);
+        if ($('#side_menu').css('margin-right') == '-400px') {
+            $('#side_menu').stop().animate({ marginRight: 0 }, 400);
         } else {
-            $('#side_menu').stop().animate({ marginRight: -400}, 400);
+            $('#side_menu').stop().animate({ marginRight: -400 }, 400);
         }
-        
     });
+
+    if ($('.swiper-slide').hasClass('swiper-slide-active')) {
+        $('.swiper-slide.swiper-slide-active .sli_tit').delay(1000).animate({ marginTop: -150 }, 800, "swing");
+        $('.swiper-slide.swiper-slide-active .sli_stit').delay(1500).animate({ marginTop: -150 }, 800, "swing");
+        $('.swiper-slide.swiper-slide-active .sli_desc').delay(2000).animate({ marginTop: -150 }, 800, "swing");
+        $('.swiper-slide.swiper-slide-active .btn_box').delay(2500).animate({ marginTop: -150 }, 800, "swing");
+    }
 
 });
