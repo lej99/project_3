554ff21ea01806e944349ebdb36501e9575f01ce
lej99 <eunji_ouo@naver.com>
2023-01-11 17:20:50 +0900

    23.01.11(0)

--- js/main.js
+++ js/main.js
@@ -7,15 +7,46 @@ $(document).ready(function () {
     $('.slider_control .pagination .swiper-pagination-bullet').eq(2)
         .html('<span class="bullet_text">DOOSUNG PAPER</span>');
 
-    $('.side_door').click(function () {
-        $('.close').toggleClass('off');
-        $('.open').toggleClass('off');
-
-        if ($('#side_menu').css('margin-right') == '-400px') {
-            $('#side_menu').stop().animate({ marginRight: 0 }, 400);
+    
+    
+        $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('header').addClass('active');
+            $('#scr_btn').stop().fadeIn();
+            $('.close').addClass('off');
+            $('.open').removeClass('off');
+            $('#side_menu').addClass('scroll').stop().animate({ right: -450 }, 400);
         } else {
-            $('#side_menu').stop().animate({ marginRight: -400 }, 400);
+            $('header').removeClass('active');
+            $('#scr_btn').stop().fadeOut();
+            $('#side_menu').removeClass('scroll').stop().animate({ right: -400 }, 400);
         }
     });
-    
+
+    $('#scr_btn').click(function () {
+        $('.close').removeClass('off');
+        $('.open').addClass('off');
+        $('#side_menu').stop().animate({ right: 0 }, 400);
+    });
+
+    //$(window).scroll(function () {
+        if ($('#side_menu').hasClass('scroll')) {
+            $('.side_door').click(function () {
+                $('.close').addClass('off');
+                $('.open').removeClass('off');
+                $('#side_menu').stop().animate({ right: -450 }, 400);
+            });
+        } else {
+            $('.side_door').click(function () {
+                $('.close').toggleClass('off');
+                $('.open').toggleClass('off');
+
+                if ($('#side_menu').css('right') == '-400px') {
+                    $('#side_menu').stop().animate({ right: 0 }, 400);
+                } else {
+                    $('#side_menu').stop().animate({ right: -400 }, 400);
+                }
+            });
+        }
+    //});
 });
